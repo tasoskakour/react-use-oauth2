d4d2c71fc66832bf2e4b1ca355f51c1c2be79b1b
Tasos Kakouris <tasoskakour@gmail.com>
2022-03-09 18:11:38 +0200

    Fix storybook env vars

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,21 @@
+function injectEnv(definitions) {
+  const env = 'process.env';
+
+  if (!definitions[env]) {
+    return {
+      ...definitions,
+      [env]: JSON.stringify(
+        Object.fromEntries(
+          Object.entries(definitions)
+            .filter(([key]) => key.startsWith(env))
+            .map(([key, value]) => [key.substring(env.length + 1), JSON.parse(value)]),
+        ),
+      ),
+    };
+  }
+  return definitions;
+}
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,8 +23,23 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    {name: "@storybook/addon-essentials",options: {
+      // https://github.com/storybookjs/storybook/issues/15901
+      // docs not compatible with webpack 5.
+      docs: false,
+  },},
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  // https://github.com/storybookjs/storybook/issues/17336#issuecomment-1022153682
+  webpackFinal: async config => {
+    const definePlugin = config.plugins.find(
+      ({ constructor }) => constructor && constructor.name === 'DefinePlugin',
+    );
+    if (definePlugin) {
+      definePlugin.definitions = injectEnv(definePlugin.definitions);
+    }
+
+    return config;
+  },
 }
