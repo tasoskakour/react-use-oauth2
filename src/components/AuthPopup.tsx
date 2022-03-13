2d8f507a96d68c1ff0055e2e2a79282e8cda3c1f
Tasos Kakouris <tasoskakour@gmail.com>
2022-03-13 07:25:15 +0200

    Add AuthPopup

--- src/components/AuthPopup.tsx
+++ src/components/AuthPopup.tsx
@@ -1,3 +1,41 @@
-const AuthPopup = () => <div>Hello world, I am a Popup</div>;
+import { useEffect } from 'react';
+import { OAUTH_RESPONSE, OAUTH_STATE_KEY } from './constants';
+import { queryToObject } from './tools';
+
+const checkState = (receivedState: string) => {
+	const state = sessionStorage.getItem(OAUTH_STATE_KEY);
+	return state === receivedState;
+};
+
+const AuthPopup = () => {
+	// On mount
+	useEffect(() => {
+		const payload = {
+			...queryToObject(window.location.search.split('?')[1]),
+			...queryToObject(window.location.hash.split('#')[1]),
+		};
+		const state = payload?.state;
+		const error = payload?.error;
+
+		if (error) {
+			window.opener.postMessage({
+				type: OAUTH_RESPONSE,
+				error: decodeURI(error) || 'OAuth error:An error has occured.',
+			});
+		} else if (state && checkState(state)) {
+			window.opener.postMessage({
+				type: OAUTH_RESPONSE,
+				payload,
+			});
+		} else {
+			window.opener.postMessage({
+				type: OAUTH_RESPONSE,
+				error: 'OAuth error: State mismatch.',
+			});
+		}
+	}, []);
+
+	return <div>Popup Loading...</div>;
+};
 
 export default AuthPopup;
