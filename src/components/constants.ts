791da1af7c6062dd71a596f4d5821ed0a1384b3c
Tasos Kakouris <tasoskakour@gmail.com>
2022-03-13 07:23:36 +0200

    Add constants

---
+++ src/components/constants.ts
@@ -0,0 +1,5 @@
+export const POPUP_HEIGHT = 700;
+export const POPUP_WIDTH = 600;
+export const OAUTH_STATE_KEY = 'react-use-oauth2-state-key';
+export const OAUTH_RESPONSE = 'react-use-oauth2-response';
+export const DEFAULT_EXCHANGE_CODE_FOR_TOKEN_METHOD = 'POST';
