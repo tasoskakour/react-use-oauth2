66089be15611815c6bda457d3f3c4debe5d8ba7e
Tasos Kakouris <tasoskakour@gmail.com>
2022-04-16 08:23:23 +0300

    Adjust example components

--- example/client/LoginAuthorizationCode.tsx
+++ example/client/LoginAuthorizationCode.tsx
@@ -1,26 +1,13 @@
 import { useOauth2 } from '../../src/components';
 
-const REACT_APP_AUTHORIZE_URL =
-	process.env.NODE_ENV === 'test'
-		? 'https://accounts.google.com/o/oauth2/v2/auth'
-		: (process.env.REACT_APP_AUTHORIZE_URL as string);
-const REACT_APP_CLIENT_ID =
-	process.env.NODE_ENV === 'test'
-		? 'some-client-id'
-		: (process.env.REACT_APP_CLIENT_ID as string);
-const REACT_APP_SCOPE =
-	process.env.NODE_ENV === 'test' ? 'some-scope' : (process.env.REACT_APP_SCOPE as string);
-
-console.log('TEST', process.env.NODE_ENV);
-
 const LoginCode = () => {
 	const { data, loading, error, getAuth } = useOauth2({
-		authorizeUrl: REACT_APP_AUTHORIZE_URL,
-		clientId: REACT_APP_CLIENT_ID,
+		authorizeUrl: 'http://localhost:3001/mock-authorize',
+		clientId: 'SOME_CLIENT_ID',
 		redirectUri: `${document.location.origin}/callback`,
-		scope: REACT_APP_SCOPE,
+		scope: 'SOME_SCOPE',
 		responseType: 'code',
-		exchangeCodeForTokenServerURL: 'http://localhost:3001/token',
+		exchangeCodeForTokenServerURL: 'http://localhost:3001/mock-token',
 		exchangeCodeForTokenMethod: 'POST',
 		onSuccess: (payload) => console.log('Success', payload),
 		onError: (error_) => console.log('Error', error_),
@@ -33,11 +20,11 @@ const LoginCode = () => {
 	}
 
 	if (loading) {
-		return <div>Loading...</div>;
+		return <div id="authorization-code-loading">Loading...</div>;
 	}
 
 	if (isLoggedIn) {
-		return <pre>{JSON.stringify(data)}</pre>;
+		return <pre id="authorization-code-data">{JSON.stringify(data)}</pre>;
 	}
 
 	return (
