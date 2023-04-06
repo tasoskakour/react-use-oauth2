56160e8550fc08de37f7302f0e6823b8da043209
Tasos Kakouris <tasoskakour@gmail.com>
2023-04-06 16:06:49 +0300

    Fix tests and build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
-import packageJson from './package.json';
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
 	{
@@ -32,7 +32,7 @@ export default [
 		],
 	},
 	{
-		input: 'dist/esm/index.d.ts',
+		input: 'dist/esm/types/index.d.ts',
 		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
 		plugins: [dts()],
 	},
