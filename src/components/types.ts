c7ed9be575d9805127c6c307a0f79e8936ba7577
Tasos Kakouris <tasoskakour@gmail.com>
2023-04-08 09:30:26 +0300

    Move types to separate file

---
+++ src/components/types.ts
@@ -0,0 +1,31 @@
+export type TAuthTokenPayload = {
+	token_type: string;
+	expires_in: number;
+	access_token: string;
+	scope: string;
+	refresh_token: string;
+};
+
+export type TResponseTypeBasedProps<TData> =
+	| {
+			responseType: 'code';
+			exchangeCodeForTokenServerURL: string;
+			exchangeCodeForTokenMethod?: 'POST' | 'GET';
+			onSuccess?: (payload: TData) => void; // TODO as this payload will be custom
+			// TODO Adjust payload type
+	  }
+	| {
+			responseType: 'token';
+			onSuccess?: (payload: TData) => void; // TODO Adjust payload type
+	  };
+
+export type TOauth2Props<TData = TAuthTokenPayload> = {
+	authorizeUrl: string;
+	clientId: string;
+	redirectUri: string;
+	scope?: string;
+	extraQueryParameters?: Record<string, any>;
+	onError?: (error: string) => void;
+} & TResponseTypeBasedProps<TData>;
+
+export type TState<TData = TAuthTokenPayload> = TData | null;
